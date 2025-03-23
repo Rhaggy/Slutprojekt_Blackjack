83c673aba9a6f15147d8d1201b5208ecff95d8d4
Gustav S <gusa07002@utb.vaxjo.se>
2025-03-23 16:53:33 +0100

    3

--- blackjack_main.py
+++ blackjack_main.py
@@ -1,13 +1,16 @@
 from tkinter import * 
 
+
 root = Tk()
 root.title("Blackjack")
-
-root.geometry("1600x1080")
+root.geometry("900x500")
 root.configure(background="Green")
 
+main_frame = Frame(root, bg="green")
+main_frame.pack(pady=20)
 
-
+dealer_frame = LabelFrame(main_frame, text="Dealer",)
+dealer_frame.grid(row=0,column=0,padx=20,ipadx=20)
 
 
 root.mainloop()
